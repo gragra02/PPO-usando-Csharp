743cbb332946f969ec66f6e9da03b21125ae9d99
Graziele da Silva <gragra02@gmail.com>
2025-06-16 19:06:00 -0300

    novos exercícios
    novos exercícios

---
+++ 05Acesso/Produto.cs
@@ -0,0 +1,8 @@
+class Produto
+{
+    /*
+    public  ->  Atributo e métido visível em qualquer classe
+    private -> Atributo e método vísiveis apenas dentro da classe onde foram criadas
+    protected -> Atributos e método visível em classes onde são criadors ou herdados  
+    */
+ }
